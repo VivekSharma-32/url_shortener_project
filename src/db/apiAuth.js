24017885c45b62d8d241d035a8398a1d093a7179
Vivek sharma <vivek.sharma1275@gmail.com>
2024-11-22 10:46:29 -0800

    changes

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -8,9 +8,8 @@ export async function login({ email, password }) {
 
   if (error) {
     throw new Error(error.message);
-
-    return data;
   }
+  return data;
 }
 
 export async function getCurrentUser() {
